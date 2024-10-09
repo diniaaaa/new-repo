e05b205f5f831f6ac02c4280f4c65576468e1e26
diniaa <diniaslepet@gmail.com>
2024-10-09 13:42:32 +0700

    Add files via upload
    Fortofolio

---
+++ script.js
@@ -0,0 +1,16 @@
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.onscroll = function() {
+    const scrollToTopButton = document.getElementById('scrollToTop');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+};
